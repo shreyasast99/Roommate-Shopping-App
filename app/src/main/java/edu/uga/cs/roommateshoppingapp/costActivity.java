a69cc2050618f0997aecd06f26096ce500391b18
Hyemi <bhm0517@hotmail.com>
2021-12-06 15:48:53 -0500

    created activity cost

---
+++ app/src/main/java/edu/uga/cs/roommateshoppingapp/costActivity.java
@@ -0,0 +1,14 @@
+package edu.uga.cs.roommateshoppingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class costActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cost);
+    }
+}
